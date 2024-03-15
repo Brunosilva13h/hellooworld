24395ef18c6aa1590d2a74b1eb7c3539a2074c47
Brunosilva13h <bruno70cardososilva@gmail.com>
2024-03-14 21:38:41 -0300

    ase

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,16 +1,16 @@
-
 /**
  * Chaves de configuração do Firebase
  * Copie aqui as suas prórias chaves do Firebase.
  **/
 const firebaseConfig = {
-    apiKey: "AIzaSyCSnZkzd2CRDI2EFQ6a_A1t5C-GnVJGM4s",
-    authDomain: "helloword-77420.firebaseapp.com",
-    projectId: "helloword-77420",
-    storageBucket: "helloword-77420.appspot.com",
-    messagingSenderId: "641177297902",
-    appId: "1:641177297902:web:3f4f39567102018b8664ff"
-  };
+    apiKey: "AIzaSyB_Dk7S89N7wQk_o3cntUH5-7v2IWd_P8Q",
+    authDomain: "hellowordblog.firebaseapp.com",
+    databaseURL: "https://hellowordblog-default-rtdb.firebaseio.com",
+    projectId: "hellowordblog",
+    storageBucket: "hellowordblog.appspot.com",
+    messagingSenderId: "238708085735",
+    appId: "1:238708085735:web:8c7fdcd4b63159275fe342"
+};
 
 // Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
@@ -66,4 +66,17 @@ function notLogged() {
     userIcon.style.display = 'inline';
     // Altera a label para entrar
     userLabel.innerHTML = 'Entrar';
+}
+
+// Função que converte datas do Firebase (timestamp) para pt-BR
+function convertTimestampToDateFormat(timestamp) {
+    const date = new Date(timestamp);
+
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const hour = date.getHours().toString().padStart(2, '0');
+    const min = date.getMinutes().toString().padStart(2, '0');
+
+    return `${day}/${month}/${year} às ${hour}:${min}`;
 }
--- profile.php
+++ profile.php
@@ -5,7 +5,7 @@ require("_global.php");
 
 // Configurações desta página
 $page = array(
-    "title" => "Perfil de Usuário",
+    "title" => "Perfil do Usuário",
     "css" => "profile.css",
     "js" => "profile.js"
 );
@@ -14,9 +14,35 @@ $page = array(
 require('_header.php');
 ?>
 
-<article></article>
+<article>
 
-<aside></aside>
+    <h2>Olá <span id="userName">usuário</span>!</h2>
+
+    <div id="userCard"></div>
+
+    <p>Sua conta é gerenciada pelo Google. Clique no botão abaixo para acessar seu perfil no Google.</p>
+
+    <p class="center">
+        <button type="button" id="btnGoogleProfile">
+            <i class="fa-brands fa-google fa-fw"></i>
+            Acessar perfil no Google
+        </button>
+    </p>
+
+    <p>Clique no botão abaixo se quise sair do aplicativo.</p>
+
+    <p class="center">
+        <button type="button" id="btnLogout">
+        <i class="fa-solid fa-right-from-bracket fa-fw"></i>
+            Logout / Sair
+        </button>
+    </p>
+
+</article>
+
+<aside>
+    <h3>+ para você</h3>
+</aside>
 
 <?php
 // Inclui o rodapé do documento
